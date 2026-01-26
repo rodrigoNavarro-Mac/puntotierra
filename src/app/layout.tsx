6e9e67f4994f1e407d4d3583e1358b09932e705f
rodrigoNavarro-Mac <navarro.marquez.rodrigo@gmail.com>
2026-01-26 14:07:38 -0600

    Googlr ANAl

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" className={`${montserrat.variable} ${inter.variable}`}>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PCTDVKNF');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body className="antialiased font-body">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PCTDVKNF"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <JsonLd />
         {children}
       </body>
